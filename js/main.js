ee36b57b0d39b96494fe3994daacb131c40e8545
GalchenkoS <105945730+GalchenkoS@users.noreply.github.com>
2023-05-18 17:14:20 +0300

    Сяоми-Химо-C26
    Сяоми-Химо-C26

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function () {
+  $(".slider, .slider__items").slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
+});
+
+$(function () {
+  $(".price__items").slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+  });
+});
